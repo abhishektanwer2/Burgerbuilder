acb109d9dd633ce2c3aa26b078cb48c831dcb18d
Abhishek Tanwer <abhishektanwer2@gmail.com>
2020-09-20 15:42:08 -0400

    merging to master

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Layout from "./components/Layout/Layout";
 import "./App.css";
-import BurgerBuilder from "./containers/BurgerBuider/BurgerBuilder";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
 class App extends Component {
   render() {
